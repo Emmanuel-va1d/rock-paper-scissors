f25693f6cc667d750c592ab3e66035f6d01238a0
emmanuel-va1d <vald.emmanuel04@gmail.com>
2023-03-23 20:20:29 -0700

    Created a function that gets the computer's random choice and tested the output in the console.

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log("test");
+let getComputerChoice = () => {
+    let choice = Math.floor(Math.random() * 3);
+    if (choice == 0)
+        return "Rock";
+    else if (choice == 1)
+        return "Paper";
+    else
+        return "Scissors";
+};
+
+console.log(getComputerChoice());
