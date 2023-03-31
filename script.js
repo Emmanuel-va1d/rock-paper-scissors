c234e68b7c0fec3dafbe5fd62d14df7ebdd41df7
emmanuel-va1d <vald.emmanuel04@gmail.com>
2023-03-31 11:21:18 -0700

    Displayed the selections for each player

--- script.js
+++ script.js
@@ -8,22 +8,32 @@ var getComputerChoice = () => {
         return "Scissors";
 };
 
+var playRound = (choice, computer) => {
+    if (choice == "Rock" && computer == "Scissors" ||
+    choice == "Paper" && computer == "Rock" ||
+    choice == "Scissors" && computer == "Paper")
+        console.log(++playerScore);
+    else if (choice == computer)
+        console.log("tie");
+    else
+        console.log(++computerScore);
+}
+
+
 var game = (choice) => {
-    console.log(choice);
     let computerChoice = getComputerChoice();
-    console.log(computerChoice);
 
     if (playerScore < 5 && computerScore < 5) {
-        if (choice == "Rock" && computerChoice == "Scissors" ||
-        choice == "Paper" && computerChoice == "Rock" ||
-        choice == "Scissors" && computerChoice == "Paper")
-            console.log(++playerScore);
-        else if (choice == computerChoice)
-            console.log("tie");
-        else
-            console.log(++computerScore);
-    } else 
-        console.log("win");
+
+        console.log(computerChoice);
+        console.log(choice);
+
+        playRound(choice, computerChoice);
+        
+        pChoice.textContent = choice;
+        cChoice.textContent = computerChoice;
+
+    }
 
     pScore.textContent = `You - ${playerScore}`;
     cScore.textContent = `${computerScore} - Comp`;
@@ -37,7 +47,6 @@ window.onload = () => {
     const rock = document.querySelector("#rock");
     const paper = document.querySelector("#paper");
     const scissors = document.querySelector("#scissors");
-    const player = document.querySelector("#pScore");
 
     rock.addEventListener("click", () => game("Rock"));
     paper.addEventListener("click", () => game("Paper"));
