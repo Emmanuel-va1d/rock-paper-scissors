f7fd7d186d81f3c5b109a2f2fc73390f35632d6f
emmanuel-va1d <vald.emmanuel04@gmail.com>
2023-03-30 19:50:54 -0700

    Added event listeners to the buttons

--- script.js
+++ script.js
@@ -8,43 +8,18 @@ let getComputerChoice = () => {
         return "Scissors";
 };
 
-let playRound = (player, computer) => {
-    // win condition
-    if ((player == "Rock" && computer == "Scissors") || 
-    (player == "Scissors" && computer == "Paper") ||
-    (player == "Paper" && computer == "Rock")) {
-        score += 2;
-        return `You win! ${player} beats ${computer}`;
-    }
-    else if (player == computer) {
-        score++;
-        return `Draw! You both chose ${player}`;
-    }
-    else 
-        return `You lose! ${computer} beats ${player}`;
-};
-
-const rock = document.querySelector("#rock");
-const paper = document.querySelector("#paper");
-const scissors = document.querySelector("#scissors");
-
-/*let game = (player, computer) => {
-    for (let i = 0; i < 5; i++) {
-        player = prompt("Choose Rock, Paper, or Scissors: ")
-        computer = getComputerChoice();
-        console.log(playRound(player, computer));
-    }
-    if (score > 5)
-        console.log("You're the winner!");
-    else if (score == 5)
-        console.log("It's a tie!");
-    else
-        console.log("The computer is the winner!");
-};*/
-
-var score = 0;
-
+let score = 0;
 let playerChoice;
 let computerChoice;
 
-game(playerChoice, computerChoice);
+window.onload = () => {
+    const rock = document.querySelector("#rock");
+    const paper = document.querySelector("#paper");
+    const scissors = document.querySelector("#scissors");
+
+    rock.addEventListener("click", () => playerChoice = "Rock");
+    paper.addEventListener("click", () => playerChoice = "Paper");
+    scissors.addEventListener("click", () => playerChoice = "Scissors");
+
+    console.log(playerChoice);
+}
