f83077ae4f86db82917d1ea5b3f76388e2f938cd
emmanuel-va1d <vald.emmanuel04@gmail.com>
2023-03-31 10:56:07 -0700

    There is a win condition, which ends the count once the score of either player reaches 5.

--- script.js
+++ script.js
@@ -8,12 +8,39 @@ var getComputerChoice = () => {
         return "Scissors";
 };
 
+var game = (choice) => {
+    console.log(choice);
+    let computerChoice = getComputerChoice();
+    console.log(computerChoice);
+
+    if (playerScore < 5 && computerScore < 5) {
+        if (choice == "Rock" && computerChoice == "Scissors" ||
+        choice == "Paper" && computerChoice == "Rock" ||
+        choice == "Scissors" && computerChoice == "Paper")
+            console.log(++playerScore);
+        else if (choice == computerChoice)
+            console.log("tie");
+        else
+            console.log(++computerScore);
+    } else 
+        console.log("win");
+
+    pScore.textContent = `You - ${playerScore}`;
+    cScore.textContent = `${computerScore} - Comp`;
+}
+
+var playerScore = 0;
+var computerScore = 0;
+
 window.onload = () => {
+    const reset = document.querySelector("#reset");
     const rock = document.querySelector("#rock");
     const paper = document.querySelector("#paper");
     const scissors = document.querySelector("#scissors");
+    const player = document.querySelector("#pScore");
 
-    rock.addEventListener("click", () => {console.log("Rock")});
-    paper.addEventListener("click", () => {console.log("Paper")});
-    scissors.addEventListener("click", () => {console.log("Scissors")});
+    rock.addEventListener("click", () => game("Rock"));
+    paper.addEventListener("click", () => game("Paper"));
+    scissors.addEventListener("click", () => game("Scissors"));
+
 };
